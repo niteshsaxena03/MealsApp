5d5a18142047886721077a2cf2f62cfbe9231c9b
Nitesh Saxena <nitesh.saxena03@gmail.com>
2024-03-17 10:05:52 +0530

    displaying the data without any styling through FLatList by making use of functions that I created

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,8 +1,9 @@
+import CategoryGridTile from "../components/CategoryGridTile";
 import { CATEGORIES } from "../data/dummy-data";
 import { FlatList } from "react-native";
 
-function renderCategoryItem(){
-    return 
+function renderCategoryItem(itemData) {
+  return <CategoryGridTile title={itemData.item.title} color={itemData.item.color}/>;
 }
 function CategoriesScreen() {
   return (
