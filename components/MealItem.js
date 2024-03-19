322a4a729342f498b734012b7a6d3e3598c79892
Nitesh Saxena <nitesh.saxena03@gmail.com>
2024-03-20 04:28:51 +0530

    adding some more styling to meal item

--- components/MealItem.js
+++ components/MealItem.js
@@ -1,17 +1,29 @@
-import { View, Text, Pressable, Image, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  Pressable,
+  Image,
+  StyleSheet,
+  Platform,
+} from "react-native";
 
 function MealItem({ title, imageURL, duration, complexity, affordability }) {
   return (
     <View style={styles.mealItem}>
-      <Pressable>
-        <View>
-          <Image source={{ uri: imageURL }} style={styles.image} />
-          <Text style={styles.title}>{title}</Text>
-        </View>
-        <View style={styles.details}>
-          <Text style={styles.detailItem}>{duration}m</Text>
-          <Text style={styles.detailItem}>{complexity.toUpperCase()}</Text>
-          <Text style={styles.detailItem}>{affordability.toUpperCase()}</Text>
+      <Pressable
+        android_ripple={{ color: "#ccc" }}
+        style={({ pressed }) => (pressed ? styles.buttonPressed : null)}
+      >
+        <View style={styles.innerContainer}>
+          <View>
+            <Image source={{ uri: imageURL }} style={styles.image} />
+            <Text style={styles.title}>{title}</Text>
+          </View>
+          <View style={styles.details}>
+            <Text style={styles.detailItem}>{duration}m</Text>
+            <Text style={styles.detailItem}>{complexity.toUpperCase()}</Text>
+            <Text style={styles.detailItem}>{affordability.toUpperCase()}</Text>
+          </View>
         </View>
       </Pressable>
     </View>
@@ -36,9 +48,10 @@ const styles = StyleSheet.create({
     backgroundColor: "white",
     elevation: 4,
     shadowColor: "black",
-    shadowOpacity: 0.40,
+    shadowOpacity: 0.4,
     shadowRadius: 16,
-    shadowOffset: { width: 0,height:2},
+    shadowOffset: { width: 0, height: 2 },
+    overflow: Platform.OS == "android" ? "hidden" : "visible",
   },
   details: {
     flexDirection: "row",
@@ -49,5 +62,12 @@ const styles = StyleSheet.create({
   detailItem: {
     marginHorizontal: 4,
     fontSize: 12,
+  },
+  innerContainer: {
+    borderRadius: 8,
+    overflow: "hidden",
+  },
+  buttonPressed: {
+    opacity: 0.5,
   },
 });
