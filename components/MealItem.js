182a5a3fa96299fc47b0ab045bbe0b5e5dcbc3d7
Nitesh Saxena <nitesh.saxena03@gmail.com>
2024-03-20 04:21:11 +0530

    adding multiple styles to multiple components of meal item

--- components/MealItem.js
+++ components/MealItem.js
@@ -1,17 +1,17 @@
 import { View, Text, Pressable, Image, StyleSheet } from "react-native";
 
-function MealItem({ title, imageURL,duration,complexity,affordability }) {
+function MealItem({ title, imageURL, duration, complexity, affordability }) {
   return (
-    <View>
+    <View style={styles.mealItem}>
       <Pressable>
         <View>
           <Image source={{ uri: imageURL }} style={styles.image} />
           <Text style={styles.title}>{title}</Text>
         </View>
-        <View>
-          <Text>{duration}m</Text>
-          <Text>{complexity.toUpperCase()}</Text>
-          <Text>{affordability.toUpperCase()}</Text>
+        <View style={styles.details}>
+          <Text style={styles.detailItem}>{duration}m</Text>
+          <Text style={styles.detailItem}>{complexity.toUpperCase()}</Text>
+          <Text style={styles.detailItem}>{affordability.toUpperCase()}</Text>
         </View>
       </Pressable>
     </View>
@@ -28,5 +28,26 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     textAlign: "center",
     fontSize: 18,
+  },
+  mealItem: {
+    margin: 16,
+    borderRadius: 8,
+    overflow: "hidden",
+    backgroundColor: "white",
+    elevation: 4,
+    shadowColor: "black",
+    shadowOpacity: 0.40,
+    shadowRadius: 16,
+    shadowOffset: { width: 0,height:2},
+  },
+  details: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 8,
+    justifyContent: "center",
+  },
+  detailItem: {
+    marginHorizontal: 4,
+    fontSize: 12,
   },
 });
