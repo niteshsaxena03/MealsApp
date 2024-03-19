be969812d13a65384ccf0c509dc828ef1784fa44
Nitesh Saxena <nitesh.saxena03@gmail.com>
2024-03-20 03:34:23 +0530

    creating a seperate component for displaying MealItem and using it by passing title prop

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,12 +1,25 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, FlatList } from "react-native";
 import { MEALS } from "../data/dummy-data";
+import MealItem from "../components/MealItem";
 
 function MealsOverviewScreen({ route }) {
   const catId = route.params.categoryId;
 
-  return(
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(catId) >= 0;
+  });
+
+  function renderMealItem(itemData) {
+    return <MealItem title={itemData.item.title}/>
+  }
+
+  return (
     <View style={styles.container}>
-      <Text>Meals Overview Screen-{catId}</Text>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.data}
+        renderItem={renderMealItem}
+      />
     </View>
   );
 }
