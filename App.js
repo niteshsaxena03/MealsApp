7ac78cf11aacf5a2e2405e1c34af7744cdee0c99
Nitesh Saxena <nitesh.saxena03@gmail.com>
2024-03-21 03:17:02 +0530

    adding the default screen styling options that will be applied to all screens

--- App.js
+++ App.js
@@ -2,18 +2,27 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import CategoriesScreen from "./screens/CategoriesScreen";
 import { NavigationContainer } from "@react-navigation/native";
-import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import MealsOverviewScreen from "./screens/MealsOverviewScreen";
 
-const Stack=createNativeStackNavigator();
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
       <StatusBar style="light" />
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="MealsCategories">
-          <Stack.Screen name="MealsCatgories" component={CategoriesScreen} />
+        <Stack.Navigator initialRouteName="MealsCategories" screenOptions={{headerStyle: { backgroundColor: "#351401" },
+              headerTintColor: "white",
+              contentStyle:{backgroundColor:'#3f2f25'}}}>
+          <Stack.Screen
+            name="MealsCatgories"
+            component={CategoriesScreen}
+            options={{
+              title: "Meal Categories",
+              
+            }}
+          />
           <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
         </Stack.Navigator>
       </NavigationContainer>
