b21350aa753a017781b9787336bd249d91741878
Nitesh Saxena <nitesh.saxena03@gmail.com>
2024-03-21 04:54:41 +0530

    creating a new component for displaying the details of all meals

--- screens/MealDetailScreen.js
+++
@@ -1,7 +0,0 @@
-import { Text } from "react-native";
-
-function MealDetailScreen({ route }) {
-  const mealId = route.params.mealId;
-  return <Text>Meal detail screen with id{mealId}</Text>;
-}
-export default MealDetailScreen;
