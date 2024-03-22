fd3ce0bdd559aac26b776a8e120cd2111c23658d
Nitesh Saxena <nitesh.saxena03@gmail.com>
2024-03-22 07:13:56 +0530

    a better way of adding button to header by using useLayoutEffect hook

--- App.js
+++ App.js
@@ -42,11 +42,7 @@ export default function App() {
           <Stack.Screen
             name="MealDetail"
             component={MealDetailScreen}
-            options={{
-              headerRight: () => {
-                return <Button title="temp"/>
-              },
-            }}
+            
           ></Stack.Screen>
         </Stack.Navigator>
       </NavigationContainer>
