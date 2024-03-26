e637895efd36ca60c317a3c24fee9783b97620f5
Nitesh Saxena <nitesh.saxena03@gmail.com>
2024-03-26 13:23:22 +0530

    using the export in redux file

--- store/redux/favorites-redux.js
+++ store/redux/favorites-redux.js
@@ -1,5 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import favoritesReducer from "./favorites";
+{
+  /*Default export is being imported*/
+}
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    favoriteMeals: favoritesReducer,
+  },
 });
