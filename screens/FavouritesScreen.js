8772a21fdd0d99b2d939982253b33772c6859202
Nitesh Saxena <nitesh.saxena03@gmail.com>
2024-03-26 12:52:41 +0530

    adding a default text on favorite screen which displays when there are not favorites available

--- screens/FavouritesScreen.js
+++ screens/FavouritesScreen.js
@@ -1,13 +1,36 @@
-import { Text } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 import MealsList from "../components/MealsList/MealsList";
 import { useContext } from "react";
 import { FavoritesContext } from "../store/context/favorites-context";
 import { MEALS } from "../data/dummy-data";
 
 function FavoritesScreen() {
-  const favoriteMealsContext=useContext(FavoritesContext);
+  const favoriteMealsContext = useContext(FavoritesContext);
 
-  const favoriteMeals=MEALS.filter(meal=>favoriteMealsContext.ids.includes(meal.id));
-  return <MealsList items={favoriteMeals}/>
+  const favoriteMeals = MEALS.filter((meal) =>
+    favoriteMealsContext.ids.includes(meal.id)
+  );
+
+  if (favoriteMeals.length === 0) {
+    return (
+      <View style={styles.rootContainer}> 
+        <Text style={styles.text}>You dont have any favorite meals yet!</Text>
+      </View>
+    );
+  }
+  return <MealsList items={favoriteMeals} />;
 }
 export default FavoritesScreen;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 18,
+    color:'white',
+    fontWeight:'bold',
+  },
+});
