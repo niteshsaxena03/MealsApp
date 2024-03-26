08b174d0800ad4c5a76f8501c2247c7f0b473935
Nitesh Saxena <nitesh.saxena03@gmail.com>
2024-03-26 12:47:11 +0530

    using the useContext hook and newly created list component in order to display the list of favorite meals

--- screens/FavouritesScreen.js
+++ screens/FavouritesScreen.js
@@ -1,6 +1,13 @@
 import { Text } from "react-native";
+import MealsList from "../components/MealsList/MealsList";
+import { useContext } from "react";
+import { FavoritesContext } from "../store/context/favorites-context";
+import { MEALS } from "../data/dummy-data";
 
 function FavoritesScreen() {
-  return <Text>This is favourite</Text>;
+  const favoriteMealsContext=useContext(FavoritesContext);
+
+  const favoriteMeals=MEALS.filter(meal=>favoriteMealsContext.ids.includes(meal.id));
+  return <MealsList items={favoriteMeals}/>
 }
 export default FavoritesScreen;
